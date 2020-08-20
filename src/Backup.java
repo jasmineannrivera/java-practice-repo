fc05ae48fed8424376c5131deeede18b876810dc
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-08-20 11:38:04 -0500

    backup

--- src/Backup.java
+++ src/Backup.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 public class Backup {
-    -----------Problem 0--------------
+//    -----------Problem 0--------------
 //produce a system that can vary its output based upon a specific count.  This output should vary
 //depending on if the count is current at 2, 3 or 6 any number divisible thereby.  If the count is
 //not at a number divisible by 2, 3 or 6 it should print the counter, otherwise print foo when the
@@ -36,7 +38,7 @@ public class Backup {
 
     }
 
------------Problem 1--------------
+//-----------Problem 1--------------
     //given a sentence, please reverse the characters within the sentence while leaving the words in the sentence. You can assume all characters are standard UTF-8 and you can ignore punctuation.  You can also assume for simplicity that you can fit the entire sentence in memory.  Lastly for simplicity, you can assume that there will only ever be one space between each word (so you don't have to worry about mismatched amounts of white space).
 //
 //        example
@@ -61,16 +63,16 @@ public class Backup {
         return reversedString;
     }
 
------------Problem 2--------------
+//-----------Problem 2--------------
 //given a sorted array list of integers, please create a balanced binary tree.  note that you can use the following helper class if you would like.  You should return the root of the tree as your result.
-
-    input: [1,2,3,4,5,6,7]
-    output:     4
-            /   \
-            2      6
-            / \    / \
-            1  3   5   7
-            (you would return the node with a value of 4)
+//
+//    input: [1,2,3,4,5,6,7]
+//    output:     4
+//            /   \
+//            2      6
+//            / \    / \
+//            1  3   5   7
+//            (you would return the node with a value of 4)
 
     //helper class
     public class Node {
